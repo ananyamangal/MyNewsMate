04af72937d0b853d1bdac0580c6e8293945557e7
Ananya Mangal <ananyamangal@Ananyas-MacBook-Air.local>
2024-09-01 17:29:32 +0530

    next

--- src/avatarselection.js
+++ src/avatarselection.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './AvatarSelection.css';
+import imagee from "./assets/Screenshot 2024-09-01 at 5.18.25 PM.png"
 import imageee from "./assets/Screenshot_2024-09-01_at_1.57.47_PM-removebg-preview (1).png"
-import imagee from "./assets/Screenshot_2024-09-01_at_1.32.02_PM-removebg-preview.png"
 const AvatarSelection = () => {
     return (
-        <div className="avatar-selection">
+
+        <div>
+              <header className="navbar">
             <nav className="navbar">
-                <div className="navbar-left">
-                    <a href="#">Home</a>
-                    <a href="#">Choose Avatar</a>
-                    <a href="#">News</a>
-                </div>
-                <div className="navbar-right">
-                    <a href="#">Login/Signup</a>
-                </div>
-            </nav>
+        <div className="navbar-left">
+            <a href="#">Home</a>
+            <a href="#">Choose Avatar</a>
+            <a href="#">News</a>
+        </div>
+        <div className="navbar-right">
+            <a href="#">Login/Signup</a>
+        </div>
+    </nav>
+    </header>
+        <div className="avatar-selection">
+            
             <div className="avatar-container">
-                <h2>Choose your avatar!</h2>
+                <h2>Choose your Avatar!</h2>
                 <div className="avatars">
                     <div className="avatar">
                         <img src={imageee} alt="Avatar 1" />
@@ -26,6 +31,7 @@ const AvatarSelection = () => {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
