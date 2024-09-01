1e4f2bbb71bdef6fd146fed731859c0d6acbf9b0
Ananya Mangal <ananyamangal@Ananyas-MacBook-Air.local>
2024-09-01 16:46:54 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./home.js";
 import AvatarSelection from './avatarselection.js';
+import Topics from './topics.js';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/lala' element={<Home></Home>}></Route>
+      <Route path='/hehe' element={<Topics></Topics>}></Route>
       <Route path='/haha' element={<AvatarSelection></AvatarSelection>}></Route>
       
     </Routes>
---
+++ src/topics.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './topics.css';
+
+
+const topics = [
+  'Sports',
+  'Bollywood',
+  'Politics',
+  'Crime',
+  'Arts',
+  'Tech',
+  'Olympics',
+  'Paralympics',
+  'Weather',
+  'International',
+  'Entertainment',
+  'Finance',
+  'Business',
+  'Trade'
+];
+
+const Topics = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Choose topics you like</h1>
+      <div className="topics-grid">
+        {topics.map((topic, index) => (
+          <div key={index} className="topic-card">
+            {topic}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Topics;
